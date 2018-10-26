7d4e3ee7a4051cb123af0a5cfeb20a6535856712
Ba-So <Bastian.Sommerfeld@gmail.com>
2018-10-26 11:21:13 +0200

    works great

--- class_parallel/classes.py
+++ class_parallel/classes.py
@@ -2,61 +2,56 @@
 # coding=utf-8
 import numpy as np
 from multiprocessing import Process, Array, Queue
+from decorators import ParallelDecorator, DebugDecorator
 
-class Operations(object):
-    self.mp = self.Mp(1, False)
+mp = ParallelDecorator.Mp(8, False)
 
+@DebugDecorator.time_this
+@ParallelDecorator.parallel_np_array(mp)
+def add_arrays(a, b):
+    result = []
+    for i, x_a in enumerate(a):
+        x_b = b[i]
+        result.append(add_skalars(x_a, x_b))
+    return result
 
-    @time_this
-    @parallel_np_array
-    def add_arrays(a, b):
-        result = []
-        for i, x_a in a:
-            x_b = b[i]
-            result.append(add_skalars(x_a, x_b))
-        return result
-
-    def add_skalars(a, b):
-        '''the tidier version, since I clearly hand over the information to be processed'''
-        result = a + b
-        return(result)
-
-class Data(Operations):
-
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-        self.result = 0
+def add_skalars(a, b):
+    result = a + b
+    return(result)
 
 class DataStack(object):
 
     def __init__(self, num_sets):
+        super(DataStack, self).__init__()
         self.num_sets = num_sets
-        self.num_procs = 3
-        self.mp = False
-        self.prepare()
+        self.prepare('a')
+        self.prepare('b')
 
-    def prepare(self):
-        self.data_list = []
+    def prepare(self, name):
+        data_list = []
         for i in range(self.num_sets):
-            self.data_list.append(data(i, i + 1))
+            data_list.append(i)
+        shrd_list = ParallelDecorator.parallel_np_array.shared_array_1D([self.num_sets])
+        shrd_list[:] = data_list[:]
+        setattr(self, name, shrd_list)
 
     def print_all(self):
-        for i, thing in enumerate(self.data_list):
-            if thing.result:
-                print(thing.result)
-            else:
-                print('nothing here {}'.format(i))
+        for i, thing in enumerate(self.result):
+            print(thing)
+
+    def execute(self):
+        result = add_arrays(self.a, self.b)
+        setattr(self, "result", result)
 
 if __name__ == '__main__':
 
     states = [False, True]
     for j in [10, 100, 1000, 10000, 100000, 1000000]:
         for state in states:
-            print(state)
+            print(mp.switch)
             ds = DataStack(j)
-            ds.mp = state
             ds.execute()
-            #ds.print_all()
+           #ds.print_all()
+            mp.toggle_switch()
 
 
--- class_parallel/decorators.py
+++ class_parallel/decorators.py
@@ -1,6 +1,53 @@
 #!/usr/bin/env python
 # coding=utf-8
-class Decorator_par(object):
+import ctypes
+import numpy as np
+from multiprocessing import Process, Queue, Array
+class DebugDecorator(object):
+    def __init__(self):
+        super(DebugDecorator, self).__init__()
+
+    class time_this(object):
+        def __init__(self, decorated):
+            self._decorated = decorated
+        def __call__(self, *args, **kwargs):
+            import datetime
+            before = datetime.datetime.now()
+            x = self._decorated(*args, **kwargs)
+            after = datetime.datetime.now()
+            print("Elapsed Time: {}".format(after - before))
+            return x
+
+    class PrintArgs(object):
+        def __init__(self, decorated):
+            self._decorated = decorated
+
+        def __call__(self, *args, **kwargs):
+            print("Name of func '{}':".format(self._decorated.__name__))
+            print("-"*10)
+            print("args: {}".format(args))
+            print("-"*10)
+            print("kwargs: {}".format(kwargs))
+            print("\n")
+            return self._decorated(*args, **kwargs)
+
+    class PrintReturn(object):
+        def __init__(self, decorated):
+            self._decorated = decorated
+
+        def __call__(self, *args, **kwargs):
+            out = self._decorated(*args, **kwargs)
+            print("return value: {}".format(out))
+            return out
+
+
+
+
+class ParallelDecorator(object):
+
+    def __init__(self, mp):
+        super(ParallelDecorator, self).__init__()
+
     class Mp(object):
         def __init__(self, num_procs, switch):
             self.num_procs = num_procs
@@ -10,18 +57,6 @@ class Decorator_par(object):
         def change_num_procs(self, num_procs):
             self.num_procs = num_procs
 
-    class time_this(object):
-        def __init__(self, decorated):
-            self._decorated = decorated
-        def __call__(self, *args, **kwargs):
-            print(args)
-            import datetime
-            before = datetime.datetime.now()
-            x = self._decorated(*args, **kwargs)
-            after = datetime.datetime.now()
-            print("Elapsed Time: {}".format(after - before))
-            return x
-
     class parallel_np_array(object):
         """do parallelisation using shared memory"""
         def __init__(self, mp=None):
@@ -29,24 +64,28 @@ class Decorator_par(object):
         def __call__(self, func):
             def _parall(*args, **kwargs):
                 if self._mp.switch:
-                    from multiprocessing import Process, Queue
-                    slices = Slicer(self._mp.num_procs)
-                    arr = [self.shared_array_1D(np.shape(arg)) for arg in args]
-                    arr = tuple(arr)
-                    result_queue = Queue()
-                    for i in self._mp.num_procs:
-                        p = Process(target=array_worker, args=(func, arr, x_slice, result_queue,))
+                    slices = self.prepare_slices(args[0], self._mp)
+                    result_queue = []
+                    processes = []
+                    for i in range(self._mp.num_procs):
+                        q = Queue()
+                        p = Process(target=self.array_worker, args=(func, args, slices[i], q,))
+                        processes.append(p)
+                        result_queue.append(q)
                     for p in processes:
                         p.run()
-                    result = [result_queue.get()]
-                    for p in processes:
-                        p.join()
-                    return result
+                    results = []
+                    for i, p in enumerate(processes):
+                        results.append(result_queue[i].get())
+                        #p.join()
+                    return results
                 else:
                     return func(*args)
             return _parall
 
         @staticmethod
+      # @DebugDecorator.PrintReturn
+      # @DebugDecorator.PrintArgs
         def shared_array_1D(shape):
             """Form shared memory 1D numpy array"""
             from multiprocessing import Array
@@ -56,25 +95,25 @@ class Decorator_par(object):
             return shared_array
 
         @staticmethod
+      # @DebugDecorator.PrintArgs
         def array_worker(func, fargs, x_slice, result_queue):
             """worker to do the slicing and all"""
-            # finish
+            args = [arg[x_slice] for arg in fargs]
+            result = func(*args)
             result_queue.put(result)
 
         @staticmethod
-        def Slicer():
-
-class DebugDecorator
-    class time_this(object):
-        def __init__(self, decorated):
-            self._decorated = decorated
-        def __call__(self, *args, **kwargs):
-            import datetime
-            print(args)
-            before = datetime.datetime.now()
-            x = self._decorated(*args, **kwargs)
-            after = datetime.datetime.now()
-            print("Elapsed Time: {}".format(after - before))
-            return x
+      # @DebugDecorator.PrintArgs
+        def prepare_slices(x_list, mp):
+            """returns appropriate slices for worker"""
+            l_shape = np.shape(x_list)
+            if mp.num_procs > 1:
+                len_slice = l_shape[0] % (mp.num_procs - 1)
+            else:
+                len_slice = l_shape[0]
+            slices = []
+            for proc in range(mp.num_procs):
+                slices.append(slice(proc * len_slice, (proc + 1) * len_slice))
+            return slices
 
 
